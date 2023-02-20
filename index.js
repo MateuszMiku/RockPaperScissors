39ba9d07f4bc30253bb45927fdb20ca56babb18f
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-02-20 15:08:32 +0100

    define a function that picks rock paper or scissors

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("hello");
+const myArray = ['rock', 'paper', 'scissors'];
+
+function getComputerChoice() {
+    const randomIndex = Math.floor(Math.random() * myArray.length);
+    return myArray[randomIndex];
+}
+
