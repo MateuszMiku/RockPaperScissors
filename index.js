6f33fe207d6784ca863afd199e97d0c02f4aa7b9
MateuszMiku <mateuszmiku.mm@gmail.com>
2023-02-20 16:32:51 +0100

    define playRound function

--- index.js
+++ index.js
@@ -5,3 +5,25 @@ function getComputerChoice() {
     return myArray[randomIndex];
 }
 
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection == computerSelection) {
+        return "The game ended as a draw"
+    } else if (playerSelection == 'rock' && computerSelection == 'paper') {
+        return "You lose, paper beats rock"
+    } else if (playerSelection == 'rock' && computerSelection == 'scissors') {
+        return "You win, rock beats scissors"
+    } else if (playerSelection == 'paper' && computerSelection == 'rock') {
+        return "You win, paper beats rock"
+    } else if (playerSelection == 'paper' && computerSelection == 'scissors') {
+        return "You lose, scissors beat paper"
+    } else if (playerSelection == 'scissors' && computerSelection == 'rock') {
+        return "You lose, rock beats scissors"
+    } else if (playerSelection == 'scissors' && computerSelection == 'paper') {
+        return "You win, scissors beat paper"
+    } 
+}
+
+const playerSelection = 'rock';
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
